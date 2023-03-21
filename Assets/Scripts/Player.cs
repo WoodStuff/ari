0b6208b27adf496c5c500ef91e93d3b621662dac
WoodStuff <woodstuffthings@gmail.com>
2023-03-21 19:50:21 +0100

    physics  :d

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -36,11 +36,11 @@ public class Player : MonoBehaviour
 
 		// left
 		RaycastHit2D hit1 = Physics2D.Raycast(rigid.position - new Vector2(0.5f, raycastOffset), Vector2.down);
-		if (!(hit1.collider == null || hit1.distance > rayDistance) && hit1.collider.gameObject.CompareTag("Wall")) left = true;
+		if (!(hit1.collider == null || hit1.distance > rayDistance) && hit1.collider.gameObject.CompareTag("Island")) left = true;
 
 		// right
 		RaycastHit2D hit2 = Physics2D.Raycast(rigid.position - new Vector2(-0.5f, raycastOffset), Vector2.down);
-		if (!(hit2.collider == null || hit2.distance > rayDistance) && hit2.collider.gameObject.CompareTag("Wall")) right = true;
+		if (!(hit2.collider == null || hit2.distance > rayDistance) && hit2.collider.gameObject.CompareTag("Island")) right = true;
 		return left || right;
 	}
 }
